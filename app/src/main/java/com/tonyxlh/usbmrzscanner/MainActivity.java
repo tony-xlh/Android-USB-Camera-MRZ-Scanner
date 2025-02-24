a4238c6e90c3947dc5e4a111a4d2f8e5ebd13448
tony-xlh <tony@dynamsoft.com>
2025-02-24 16:00:24 +0800

    add camera preview

--- app/src/main/java/com/tonyxlh/usbmrzscanner/MainActivity.java
+++ app/src/main/java/com/tonyxlh/usbmrzscanner/MainActivity.java
@@ -1,6 +1,13 @@
 package com.tonyxlh.usbmrzscanner;
 
+import android.graphics.SurfaceTexture;
+import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,141 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.serenegiant.usb.CameraDialog;
+import com.serenegiant.usb.USBMonitor;
+import com.serenegiant.usb.UVCCamera;
+import com.serenegiant.usbcameracommon.UVCCameraHandler;
+import com.serenegiant.widget.CameraViewInterface;
 
+public class MainActivity extends AppCompatActivity  implements CameraDialog.CameraDialogParent {
+    /**
+     * set true if you want to record movie using MediaSurfaceEncoder
+     * (writing frame data into Surface camera from MediaCodec
+     *  by almost same way as USBCameratest2)
+     * set false if you want to record movie using MediaVideoEncoder
+     */
+    private static final boolean USE_SURFACE_ENCODER = false;
+    /**
+     * preview resolution(width)
+     * if your camera does not support specific resolution and mode,
+     * {@link UVCCamera#setPreviewSize(int, int, int)} throw exception
+     */
+    private static final int PREVIEW_WIDTH = 640; // 640
+    /**
+     * preview resolution(height)
+     * if your camera does not support specific resolution and mode,
+     * {@link UVCCamera#setPreviewSize(int, int, int)} throw exception
+     */
+    private static final int PREVIEW_HEIGHT = 480; //480
+    /**
+     * preview mode
+     * if your camera does not support specific resolution and mode,
+     * {@link UVCCamera#setPreviewSize(int, int, int)} throw exception
+     * 0:YUYV, other:MJPEG
+     */
+    private static final int PREVIEW_MODE = 0; // YUV
+    private UVCCameraHandler mCameraHandler;
+
+    private CameraViewInterface mUVCCameraView;
+    private ImageButton mCameraButton;
+    private USBMonitor mUSBMonitor;
+    private TextView resultTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        resultTextView = findViewById(R.id.resultTextView);
+        mCameraButton = findViewById(R.id.imageButton);
+        mCameraButton.setOnClickListener(mOnClickListener);
+        final View view = findViewById(R.id.camera_view);
+        mUVCCameraView = (CameraViewInterface) view;
+        mUVCCameraView.setAspectRatio(PREVIEW_WIDTH / (double) PREVIEW_HEIGHT);
+        mUSBMonitor = new USBMonitor(this, mOnDeviceConnectListener);
+        mCameraHandler = UVCCameraHandler.createHandler(this, mUVCCameraView,
+                USE_SURFACE_ENCODER ? 0 : 1, PREVIEW_WIDTH, PREVIEW_HEIGHT, PREVIEW_MODE);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(final View view) {
+            if ((mCameraHandler != null) && !mCameraHandler.isOpened()) {
+                CameraDialog.showDialog(MainActivity.this);
+            } else {
+                mCameraHandler.close();
+            }
+        }
+    };
+
+    private final USBMonitor.OnDeviceConnectListener mOnDeviceConnectListener = new USBMonitor.OnDeviceConnectListener() {
+        @Override
+        public void onAttach(final UsbDevice device) {
+            Toast.makeText(MainActivity.this, "USB_DEVICE_ATTACHED", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onConnect(final UsbDevice device, final USBMonitor.UsbControlBlock ctrlBlock, final boolean createNew) {
+            if (mCameraHandler != null) {
+                mCameraHandler.open(ctrlBlock);
+                startPreview();
+            }
+
+        }
+
+        @Override
+        public void onDisconnect(final UsbDevice device, final USBMonitor.UsbControlBlock ctrlBlock) {
+
+        }
+        @Override
+        public void onDettach(final UsbDevice device) {
+            Toast.makeText(MainActivity.this, "USB_DEVICE_DETACHED", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCancel(final UsbDevice device) {
+        }
+    };
+
+    private void startPreview() {
+        if (mCameraHandler != null) {
+            final SurfaceTexture st = mUVCCameraView.getSurfaceTexture();
+            mCameraHandler.startPreview(new Surface(st));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mUSBMonitor.register();
+    }
+
+    @Override
+    protected void onStop() {
+        mUSBMonitor.unregister();
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mUSBMonitor != null) {
+            mUSBMonitor.destroy();
+            mUSBMonitor = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public USBMonitor getUSBMonitor() {
+        return mUSBMonitor;
+    }
+
+    @Override
+    public void onDialogResult(boolean b) {
+
     }
 }
